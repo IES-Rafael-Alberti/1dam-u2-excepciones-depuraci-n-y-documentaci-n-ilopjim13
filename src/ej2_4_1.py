e661395a29d0b1358e991ccdca0c32a4e8f7327c
ivanjim13 <ivangorrion57@gmail.com>
2023-11-11 22:54:54 +0100

    p2_4 con dos funciones

--- src/ej2_4_1.py
+++ src/ej2_4_1.py
@@ -1,18 +1,19 @@
-
-
-
-def ordenarLista(a):
-    total = len(a)
-    rango = 0
+def ordenarMenorMayor(a, total, rango):
     o = 0
-    for i in range(0, len(a)):
-        for j in range(0, total - 1):
+    for j in range(0, total - 1):
             if a[rango] > a[rango +1]:
                 o = a[rango]
                 a[rango] = a[rango + 1]
                 a[rango + 1] = o
             rango += 1
-        rango=0
+    return a
+
+
+def ordenarLista(a):
+    total = len(a)
+    rango = 0
+    for i in range(0, len(a)):
+        ordenarMenorMayor(a, total, rango)
         total -= 1
     return a
 
